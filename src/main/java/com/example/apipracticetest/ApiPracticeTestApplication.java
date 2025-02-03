fab12cde6da616c070ff4234b70506bcc7c2b5cb
hyanyul <tjdwl@gmail.com>
2025-02-03 09:36:00 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/apipracticetest/ApiPracticeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.apipracticetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPracticeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPracticeTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apipracticetest/ApiPracticeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apipracticetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPracticeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
